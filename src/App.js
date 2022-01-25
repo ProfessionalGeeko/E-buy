741b77e64394712486bd52af3f6c88fb56b8f616
ProfessionalGeeko <pranjal366@gmail.com>
2022-01-25 23:40:16 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.componenet";
+
+
 import { Route, Routes } from 'react-router-dom'
 import withRouter from "./utils/withRouter";
 
@@ -9,6 +12,7 @@ import withRouter from "./utils/withRouter";
 
 function App() {
   return <div>
+      <Header />
       <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
---
+++ src/components/header/header.componenet.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../assets/crown.svg';
+
+import './header.style.scss'
+
+
+const Header = () => (
+    <div className='header'>
+        <Link to='/'>
+            <Logo className='logo-container' to='/' />
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                SHOP
+            </Link>
+            <Link className='option' to='/contact'>
+                CONTACT
+            </Link>
+        </div>
+    </div>
+);
+
+
+export default Header;
